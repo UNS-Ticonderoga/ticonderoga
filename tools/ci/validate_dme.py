a3682bea075cb93056adb6a82056a9eb5e9b8f78
DisturbHerb <66253095+DisturbHerb@users.noreply.github.com>
2026-06-30 19:45:05 +1000

    hopepfully fix misc checks * non-modularized changes to `check_grep.sh` and `check_grep_secret.sh` for validating modularized code and maps * dme validation to exclude modular maps

--- tools/ci/validate_dme.py
+++ tools/ci/validate_dme.py
@@ -25,6 +25,9 @@ FORBID_INCLUDE = [
 
     # Included by map.dm (which we have to whitelist)
     r'maps**/*.dm',
+# TICONDEROGA CHANGE
+	r'modular_ticonderoga/maps**/*.dm',
+# END TICONDEROGA CHANGE
 
     # OpenDream pragma lint file
     r'tools/ci/od_lints.dm'
